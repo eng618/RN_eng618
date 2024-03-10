497217cec8acab831f926233cd596e5dac2d9bcc
Eric N. Garcia <eng618@garciaericn.com>
2024-03-10 00:38:30 -0500

    feat(ProjectsList): add component to list projects
    This component takes in a local data.json file that has sections, and an
    array of items. it renders the list in a SectionList.

--- App.js
+++ App.js
@@ -1,35 +1,53 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+import ProjectsList from "./components/ProjectsList/ProjectsList";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.window}>
       <StatusBar style="auto" />
-      <Text style={[styles.text, styles.hero]}>Eric Garcia</Text>
+      <Text style={[styles.hero]}>Eric Garcia</Text>
       <Text style={styles.text}>eng618</Text>
-      <Text style={styles.body}>
-        This is my app. There are many like it, but this one is mine.
+      <Text style={styles.credo}>
+        This is my app. {"\n"}There are many like it, but this one is mine.
       </Text>
+      <ProjectsList />
     </SafeAreaView>
   );
 }
 
+// App colors
+// Background Color (#505050)
+// Accent Color (#9C8BFF)
+// Lighter Accent Color (#BDAEFF)
+// Darker Accent Color (#695DD6)
+// Complementary Color (#FFA38B)
+
+// Text color options
+// White (#FFFFFF) - For main body text on dark backgrounds, this will provide strong contrast and readability.
+// Light Grey (#CCCCCC) - For secondary text or labels, this color can provide a softer contrast with the background while still maintaining readability.
+// Off-White (#F5F5F5) - Another option for body text on dark backgrounds, offering a slightly warmer tone compared to pure white.
+// Dark Grey (#333333) - For text on lighter backgrounds or as an alternative to pure black for better readability and contrast.
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#505050",
-    alignItems: "center",
-    justifyContent: "top",
+  credo: {
+    color: "#F5F5F5",
+    fontWeight: "bold",
+    padding: 16,
+    textAlign: "center",
   },
   hero: {
+    alignItems: "center",
+    color: "#9C8BFF",
     fontSize: 24,
   },
   text: {
     color: "#9C8BFF",
   },
-  body: {
-    color: "#fff",
-    padding: 10,
-    margin: 10,
+  window: {
+    alignItems: "center",
+    backgroundColor: "#505050",
+    flex: 1,
+    justifyContent: "top",
   },
 });
---
+++ components/ProjectsList/ProjectsList.js
@@ -0,0 +1,57 @@
+import { SectionList, StyleSheet, Text, Linking, View } from "react-native";
+import Data from "./data.json";
+
+const ProjectsList = () => {
+  return (
+    <>
+      <SectionList
+        style={styles.container}
+        sections={Data}
+        keyExtractor={(item, index) => item + index}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.description}>{item.description}</Text>
+            <Text style={styles.url} onPress={() => Linking.openURL(item.url)}>
+              {item.url}
+            </Text>
+          </View>
+        )}
+      />
+    </>
+  );
+};
+
+export default ProjectsList;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    paddingTop: 0,
+  },
+  description: {
+    color: "#F5F5F5",
+  },
+  header: {
+    backgroundColor: "black",
+    color: "#9C8BFF",
+    fontSize: 32,
+    padding: 16,
+  },
+  item: {
+    backgroundColor: "#333333",
+    marginVertical: 8,
+    padding: 16,
+  },
+  title: {
+    color: "#F5F5F5",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  url: {
+    color: "#FFA38B",
+  },
+});
